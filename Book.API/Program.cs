386db8dc0c9ed182a56afb81edaee09e2c0d4d32
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-05-30 17:45:03 +0200

    Annotation Fix
    - project name Book.API changed to BookAPI
    (It was causing issues when importing class Book - thinking it's a namespace)

--- Book.API/Program.cs
+++ Book.API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Book.API
+namespace BookAPI
 {
 	public class Program
 	{
