9156bb96297669a407643ee4eb2ee96538d8dcdb
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-06-02 02:35:47 +0200

    Fixing Namespaces

--- Book.API/Entities/Book.cs
+++ Book.API/Entities/Book.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BooksAPI.Entities
+namespace BookAPI.Entities
 {
 	[Table("Books")]
     public class Book
