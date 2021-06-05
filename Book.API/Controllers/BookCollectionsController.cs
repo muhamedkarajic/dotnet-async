96bf5e9a360da7ad2c11c60094e053f3f7973286
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-06-05 15:01:33 +0200

    Post Collections Controller

---
+++ Book.API/Controllers/BookCollectionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookAPI.Models;
+using BookAPI.Services;
+using Books.API.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+	[ApiController]
+	[Route("api/bookcollections")]
+	public class BookCollectionsController : ControllerBase
+	{
+		private readonly IBookRepository _bookRepository;
+		private readonly IMapper _mapper;
+
+		public BookCollectionsController(IBookRepository bookRepository, IMapper mapper)
+		{
+			_bookRepository = bookRepository ?? throw new ArgumentNullException(nameof(bookRepository));
+			_mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateBookCollection(IEnumerable<BookForCreation> booksForCreation)
+		{
+			var books = _mapper.Map<IEnumerable<Entities.Book>>(booksForCreation);
+
+			foreach (var book in books)
+				_bookRepository.AddBook(book);
+
+			await _bookRepository.SaveChangesAsync();
+
+			return Ok();
+		}
+
+	}
+}
